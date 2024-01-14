c1934b3695b3ed84fcca110fd73db42825151d03
tkmct <1220t.takamichi@gmail.com>
2024-01-14 13:48:54 +0900

    Impl Block128
    - Encryption
    - Decryption
    - from_bytes
    - as_bytes

--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,10 @@
+use aes::cipher::{generic_array::GenericArray, BlockCipher, BlockDecrypt, BlockEncrypt, KeyInit};
+use aes::Aes256;
 use ark_ec::CurveConfig;
 use ark_ed25519::{EdwardsConfig, EdwardsProjective};
+
+#[derive(thiserror::Error, Debug)]
+enum BlockError {}
 
 pub type Zp = <EdwardsConfig as CurveConfig>::ScalarField;
 pub type G = EdwardsProjective;
@@ -10,9 +15,9 @@ pub trait IsZero {
 
 pub trait Block {
     // Encrypt in-place using AES256
-    fn encrypt(&mut self, key: &[u8; 32]);
+    fn encrypt(&self, key: &[u8; 32]) -> Self;
 
-    fn decrypt(&mut self, key: &[u8; 32]);
+    fn decrypt(&self, key: &[u8; 32]) -> Self;
 
     fn as_bytes(&self) -> &[u8];
 
@@ -24,20 +29,42 @@ pub trait Block {
 pub struct Block128([u8; 16]);
 
 impl Block for Block128 {
-    fn encrypt(&mut self, key: &[u8; 32]) {
-        todo!()
+    fn encrypt(&self, key: &[u8; 32]) -> Self {
+        let g_key = GenericArray::from(*key);
+        let cipher = Aes256::new(&g_key);
+
+        let mut g_val = GenericArray::from(self.0);
+        cipher.encrypt_block(&mut g_val);
+
+        Self(g_val.try_into().unwrap())
     }
 
-    fn decrypt(&mut self, key: &[u8; 32]) {
-        todo!()
+    fn decrypt(&self, key: &[u8; 32]) -> Self {
+        let g_key = GenericArray::from(*key);
+        let cipher = Aes256::new(&g_key);
+
+        let mut g_val = GenericArray::from(self.0);
+        cipher.decrypt_block(&mut g_val);
+
+        Self(g_val.try_into().unwrap())
     }
 
     fn as_bytes(&self) -> &[u8] {
-        todo!()
+        &self.0
     }
 
+    /// Convert slice of byte into 128bit chunked block.
+    /// If the length of the input is larger than 16, it uses the first 128bit and construct Block.
+    /// If the length of the input is less than 16, the block will be padded with zeros.
     fn from_bytes(bytes: &[u8]) -> Self {
-        todo!()
+        // check input length
+        if bytes.len() < 16 {
+            let mut dst = [0u8; 16];
+            dst[..bytes.len()].copy_from_slice(bytes);
+            Self(dst)
+        } else {
+            Self(bytes[0..16].try_into().unwrap())
+        }
     }
 }
 
@@ -63,11 +90,11 @@ impl From<[u8; 16]> for Block128 {
 pub struct Block256([Block128; 2]);
 
 impl Block for Block256 {
-    fn encrypt(&mut self, key: &[u8; 32]) {
+    fn encrypt(&self, key: &[u8; 32]) -> Self {
         todo!()
     }
 
-    fn decrypt(&mut self, key: &[u8; 32]) {
+    fn decrypt(&self, key: &[u8; 32]) -> Self {
         todo!()
     }
 
@@ -110,5 +137,19 @@ mod tests {
 
         let block = Block256::from(val);
         assert!(block.is_zero());
+    }
+
+    #[test]
+    fn test_block128_from_bytes() {
+        let val = [1u8; 12];
+        let block = Block128::from_bytes(&val);
+        assert_eq!(block.0, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,]);
+    }
+
+    #[test]
+    fn test_block128_from_bytes_2() {
+        let val = [1u8; 18];
+        let block = Block128::from_bytes(&val);
+        assert_eq!(block.0, [1u8; 16]);
     }
 }
