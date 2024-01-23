230e0aa3977dafcb7ed2bb310e51ffaa50e3044c
tkmct <1220t.takamichi@gmail.com>
2024-01-23 12:09:24 +0900

    Clean up comments

--- src/ot/extension/iknp.rs
+++ src/ot/extension/iknp.rs
@@ -5,17 +5,16 @@ use rand::prelude::{thread_rng, ThreadRng};
 use sha3::{Digest, Keccak256};
 
 use crate::{
-    block::{Block, Block128},
+    block::Block,
     channel::AbstractChannel,
     ot::{OTReceiver, OTResult, OTSender},
 };
 
 /// Key size. Base of will be performed for K times to send M keys.
-// TODO: Fix to 128 later
-const K: usize = 2;
+const K: usize = 10;
 
 // TODO: make this B generics cleaner
-fn ot_ext_send<
+pub fn ot_ext_send<
     Receiver: OTReceiver,
     T: Block,
     B: Block + Clone + Default,
@@ -30,29 +29,18 @@ fn ot_ext_send<
     // Ext sender acts as an OT receiver
     // Sample K-bits
     let mut rng = thread_rng();
-    // let s_choices = (0..K).map(|_| rng.gen::<bool>()).collect::<Vec<_>>();
-    //
-    // sender supposed to receive t for 0-th column, u for 1-st column for testing
-    let s_choices = vec![true, false];
-
-    println!("s: {:?}", s_choices);
-    // println!("ot-ext-send M: {}", M);
-    // println!("ot-ext-send M/8+1 = {}", M / 8 + 1);
+    let s_choices = (0..K).map(|_| rng.gen::<bool>()).collect::<Vec<_>>();
 
     // Perform K OT to receive K M-bits column
     let mut q_matrix = vec![vec![false; K]; M];
     for (i, s) in s_choices.iter().enumerate() {
         let received = receiver.receive::<2, B, ThreadRng>(*s as usize, &mut rng)?;
-        // println!("received {:?}", received);
         // convert received block back to M bit vec
         let bytes = &received.as_bytes()[0..(M / 8 + 1)];
-        println!("Received: {:?}", bytes);
 
         for j in 0..M {
             let l = j / 8;
             let byte = bytes[l];
-
-            println!("{}-th bit of q is {}", j, byte >> (7 - j % 8) & 1);
             q_matrix[j][i] = (byte >> (7 - j % 8) & 1) != 0;
         }
     }
@@ -64,9 +52,7 @@ fn ot_ext_send<
             let mut hasher_0 = Keccak256::default();
             let mut k_0_bytes = vec![0u8; K];
 
-            println!("compute key for k0");
             for (i, &bit) in row.iter().enumerate() {
-                println!("bit: {:?}", bit);
                 let byte = i / 8;
                 let shift = 7 - i % 8;
                 k_0_bytes[byte] |= (bit as u8) << shift;
@@ -74,13 +60,10 @@ fn ot_ext_send<
             hasher_0.update(k_0_bytes);
             let k0 = hasher_0.finalize();
 
-            println!("compute key for k1");
             let mut hasher_1 = Keccak256::default();
             let mut k_1_bytes = vec![0u8; K];
             for (i, (&bit, s)) in row.iter().zip(&s_choices).enumerate() {
-                println!("bit: {:?}, s: {:?}", bit, s);
                 let bit = bit ^ s;
-                println!("bit ^ s: {:?}", bit);
                 let byte = i / 8;
                 let shift = 7 - i % 8;
                 k_1_bytes[byte] |= (bit as u8) << shift;
@@ -94,9 +77,6 @@ fn ot_ext_send<
 
     // Send all the encrypted values
     for (v, (k0, k1)) in values.iter().zip(keys) {
-        println!("Encryption Keys:");
-        println!("key0: {:?}", &k0[..10]);
-        println!("key1: {:?}", &k1[..10]);
         channel.write_bytes(&v[0].encrypt(&k0.into()).as_bytes())?;
         channel.flush()?;
         channel.write_bytes(&v[1].encrypt(&k1.into()).as_bytes())?;
@@ -115,39 +95,31 @@ fn ot_ext_receive<
     B: Block + Clone,
     const M: usize,
     C: AbstractChannel,
-    R: Rng,
 >(
     sender: &mut Sender,
     choices: [bool; M],
     channel: &mut C,
-    rng: &mut R,
 ) -> OTResult<[T; M]> {
     // ) -> OTResult<()> {
     // K OT for M-bits messages where K is key length = 128.
     // Ext receiver acts as an OT sender
     // 1. create M * K matrix where all the values for i'th row are choice bit b_i.
     let b_matrix = choices.iter().map(|&b_i| vec![b_i; K]).collect::<Vec<_>>();
-    println!("b_matrix: {:?}", b_matrix);
 
     // 2. Sample random M * K matrix t_matrix to form share of b_matrix.
     // t_matrix ^ u_matrix = b_matrix
     let mut rng = thread_rng();
-    // let t_matrix = (0..M)
-    //     .map(|_| {
-    //         let t_i: u128 = rng.gen();
-    //         let mut row = [false; K];
-    //         row.iter_mut()
-    //             .enumerate()
-    //             .take(K)
-    //             .map(|(i, _)| (t_i >> i) & 1 == 1)
-    //             .collect::<Vec<_>>()
-    //     })
-    //     .collect::<Vec<_>>();
-
-    // For testing purpose
-    let t_matrix = vec![vec![true, true], vec![false, true]];
-
-    println!("t_matrix: {:?}", t_matrix);
+    let t_matrix = (0..M)
+        .map(|_| {
+            let t_i: u128 = rng.gen();
+            let mut row = [false; K];
+            row.iter_mut()
+                .enumerate()
+                .take(K)
+                .map(|(i, _)| (t_i >> i) & 1 == 1)
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
 
     let u_matrix = b_matrix
         .iter()
@@ -160,8 +132,6 @@ fn ot_ext_receive<
                 .collect::<Vec<_>>()
         })
         .collect::<Vec<_>>();
-
-    println!("u_matrix: {:?}", u_matrix);
 
     // perform K OT to send K-columns either (t_col, u_col)
     // send M-bits as Vec<Block128>
@@ -185,12 +155,6 @@ fn ot_ext_receive<
             u_bytes[byte] |= (bit as u8) << shift;
         }
         let u_block = B::from_bytes(&u_bytes);
-
-        // println!("t_block: {:?}", &t_block);
-        // println!("u_block: {:?}", &u_block);
-
-        // sender.send([t_block, u_block])?;
-
         sender.send([t_block, u_block])?;
     }
 
@@ -210,9 +174,6 @@ fn ot_ext_receive<
         let mut hasher = Keccak256::default();
         hasher.update(k_bytes);
         let key = hasher.finalize();
-
-        println!("Decryption Key:");
-        println!("key: {:?}", &key[..10]);
 
         let d = T::default();
         let mut v0_bytes = vec![0u8; d.bytes_len()];
@@ -242,22 +203,14 @@ mod tests {
         thread,
     };
 
-    use rand::{prelude::thread_rng, rngs::ThreadRng};
+    use rand::prelude::thread_rng;
 
     use super::*;
     use crate::{
         block::*,
         channel::Channel,
-        ot::{
-            co15::{CO15Receiver, CO15Sender},
-            OTError,
-        },
+        ot::co15::{CO15Receiver, CO15Sender},
     };
-
-    use ark_ec::twisted_edwards::TECurveConfig;
-    use ark_ed25519::EdwardsConfig;
-    use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
-    use ark_std::UniformRand;
 
     #[test]
     fn test_ot_extension() -> Result<(), Box<dyn std::error::Error>> {
@@ -271,10 +224,8 @@ mod tests {
             let writer = BufWriter::new(ot_sender_stream);
             let sender_channel = Channel::new(reader, writer);
             let mut ot_sender = CO15Sender::setup(sender_channel, &mut rng).unwrap();
-            // TODO: fix later
             let choices = [
-                true, false,
-                //  true, true, false, true, true, false, true, false,
+                true, false, true, true, false, true, true, false, true, false,
             ];
 
             let reader = BufReader::new(ext_receiver_stream.try_clone().unwrap());
@@ -284,12 +235,10 @@ mod tests {
             ot_ext_receive::<
                 CO15Sender<Channel<BufReader<UnixStream>, BufWriter<UnixStream>>>,
                 Block128,
-                // TODO: fix later
                 [Block128; 1],
-                2,
+                10,
                 Channel<BufReader<UnixStream>, BufWriter<UnixStream>>,
-                ThreadRng,
-            >(&mut ot_sender, choices, &mut ext_receiver_chan, &mut rng)
+            >(&mut ot_sender, choices, &mut ext_receiver_chan)
         });
 
         // Prepare sender
@@ -298,18 +247,17 @@ mod tests {
         let receiver_channel = Channel::new(reader, writer);
         let mut ot_receiver = CO15Receiver::setup(receiver_channel).unwrap();
 
-        // TODO: fix later
-        let values: [[Block128; 2]; 2] = [
+        let values = [
             [Block128::from(1), Block128::from(100)],
             [Block128::from(1), Block128::from(100)],
-            // [Block128::from(1), Block128::from(100)],
-            // [Block128::from(1), Block128::from(100)],
-            // [Block128::from(1), Block128::from(100)],
-            // [Block128::from(1), Block128::from(100)],
-            // [Block128::from(1), Block128::from(100)],
-            // [Block128::from(1), Block128::from(100)],
-            // [Block128::from(1), Block128::from(100)],
-            // [Block128::from(1), Block128::from(100)],
+            [Block128::from(1), Block128::from(100)],
+            [Block128::from(1), Block128::from(100)],
+            [Block128::from(1), Block128::from(100)],
+            [Block128::from(1), Block128::from(100)],
+            [Block128::from(1), Block128::from(100)],
+            [Block128::from(1), Block128::from(100)],
+            [Block128::from(1), Block128::from(100)],
+            [Block128::from(1), Block128::from(100)],
         ];
         let reader = BufReader::new(ext_sender_stream.try_clone().unwrap());
         let writer = BufWriter::new(ext_sender_stream);
@@ -320,25 +268,25 @@ mod tests {
             Block128,
             // TODO: fix later
             [Block128; 1],
-            2,
+            10,
             Channel<BufReader<UnixStream>, BufWriter<UnixStream>>,
         >(&mut ot_receiver, values, &mut ext_sender_chan)?;
 
         let receiver_result = receiver_handle.join().unwrap();
         assert!(receiver_result.is_ok());
 
-        // TODO: fix later
+        // choice for: true, false, true, true, false, true, true, false, true, false,
         let expected_result = [
             Block128::from(100),
             Block128::from(1),
-            // Block128::from(1),
-            // Block128::from(1),
-            // Block128::from(100),
-            // Block128::from(1),
-            // Block128::from(1),
-            // Block128::from(100),
-            // Block128::from(1),
-            // Block128::from(100),
+            Block128::from(100),
+            Block128::from(100),
+            Block128::from(1),
+            Block128::from(100),
+            Block128::from(100),
+            Block128::from(1),
+            Block128::from(100),
+            Block128::from(1),
         ];
 
         assert_eq!(receiver_result.unwrap(), expected_result);
