a453921974b2e95440473d29dc8480dc146eaf33
tkmct <1220t.takamichi@gmail.com>
2024-01-25 20:11:16 +0900

    Fix trait, test skelton

--- src/vole/ot_vole.rs
+++ src/vole/ot_vole.rs
@@ -1,37 +1,50 @@
-//! This module implement VOLE from oblivious transfer.
+//! This module implement VOLE from oblivious transfer and learning parity with noise assumption.
 
-use crate::AbstractChannel;
+use crate::{
+    ot::{OTReceiver, OTSender},
+    AbstractChannel,
+};
 
-struct OleLeader<C: AbstractChannel> {
+/// Vole sender inputs delta and outputs k
+/// where y = k + x * Δ
+struct VoleSender<C: AbstractChannel, Sender: OTSender> {
+    ot_sender: Sender,
     channel: C,
 }
 
-impl<C: AbstractChannel> OleLeader<C> {
-    fn new(channel: C) -> Self {
-        Self { channel }
+impl<C: AbstractChannel, Sender: OTSender> VoleSender<C, Sender> {
+    fn new(ot_sender: Sender, channel: C) -> Self {
+        Self { channel, ot_sender }
     }
 
-    fn execute(&mut self) {
+    fn send(&mut self) {
         todo!()
     }
 }
 
-struct OleFollower<C: AbstractChannel> {
+/// Vole receiver inputs x and outputs y
+/// where y = k + x * Δ
+struct VoleReceiver<C: AbstractChannel, Receiver: OTReceiver> {
+    ot_receiver: Receiver,
     channel: C,
 }
 
-impl<C: AbstractChannel> OleFollower<C> {
-    fn new(channel: C) -> Self {
-        Self { channel }
+impl<C: AbstractChannel, Receiver: OTReceiver> VoleReceiver<C, Receiver> {
+    fn new(ot_receiver: Receiver, channel: C) -> Self {
+        Self {
+            channel,
+            ot_receiver,
+        }
     }
 
-    fn execute(&mut self) {
+    fn receive(&mut self) {
         todo!()
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use rand::prelude::thread_rng;
     use std::{
         io::{BufReader, BufWriter},
         os::unix::net::UnixStream,
@@ -39,29 +52,42 @@ mod tests {
     };
 
     use super::*;
-    use crate::channel::Channel;
+    use crate::{channel::Channel, ot::co15::*};
 
     #[test]
     fn test_ole() {
-        let (leader_stream, follower_stream) = UnixStream::pair().unwrap();
+        let (sender_stream, receiver_stream) = UnixStream::pair().unwrap();
+        let (ot_sender_stream, ot_receiver_stream) = UnixStream::pair().unwrap();
 
-        let leader_thread = thread::spawn(|| {
-            let reader = BufReader::new(leader_stream.try_clone().unwrap());
-            let writer = BufWriter::new(leader_stream);
-            let leader_chan = Channel::new(reader, writer);
+        let sender_thread = thread::spawn(|| {
+            let mut rng = thread_rng();
 
-            let mut alice = OleLeader::new(leader_chan);
-            alice.execute()
+            let reader = BufReader::new(sender_stream.try_clone().unwrap());
+            let writer = BufWriter::new(sender_stream);
+            let sender_chan = Channel::new(reader, writer);
+
+            let ot_reader = BufReader::new(ot_sender_stream.try_clone().unwrap());
+            let ot_writer = BufWriter::new(ot_sender_stream);
+            let ot_sender_channel = Channel::new(ot_reader, ot_writer);
+            let ot_sender = CO15Sender::setup(ot_sender_channel, &mut rng).unwrap();
+
+            let mut alice = VoleSender::new(ot_sender, sender_chan);
+            alice.send()
         });
 
-        let reader = BufReader::new(follower_stream.try_clone().unwrap());
-        let writer = BufWriter::new(follower_stream);
-        let follower_chan = Channel::new(reader, writer);
+        let reader = BufReader::new(receiver_stream.try_clone().unwrap());
+        let writer = BufWriter::new(receiver_stream);
+        let receiver_chan = Channel::new(reader, writer);
 
-        let mut bob = OleFollower::new(follower_chan);
-        bob.execute();
+        let ot_reader = BufReader::new(ot_receiver_stream.try_clone().unwrap());
+        let ot_writer = BufWriter::new(ot_receiver_stream);
+        let ot_receiver_channel = Channel::new(ot_reader, ot_writer);
+        let ot_receiver = CO15Receiver::setup(ot_receiver_channel).unwrap();
+
+        let mut bob = VoleReceiver::new(ot_receiver, receiver_chan);
+        bob.receive();
 
         // follower things to do
-        let leader_result = leader_thread.join().unwrap();
+        let sender_result = sender_thread.join().unwrap();
     }
 }
